c3f19a39067e45aeedc7e11557f27381ab9fdaf5
XavierVill2525 <76183619+XavierVill2525@users.noreply.github.com>
2022-12-13 10:26:02 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -3,8 +3,8 @@ module.exports = {
     database: {
         connectionLimit: 10,
         host: 'localhost',
-        user: 'root',
-        password: 'root',
+        user: '46238148',
+        password: '46238148',
         database: 'db_links'
     }
 
