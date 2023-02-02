440be291453ad01525782ab5b4b9c36048dc9f1a
XavierVill2525 <76183619+XavierVill2525@users.noreply.github.com>
2023-02-01 20:53:42 -0500

    Update auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,8 +1,9 @@
 module.exports = {
-    isLoggedIn (req, res, next) {
+    isLoggedIn(req, res, next) {
         if (req.isAuthenticated()) {
             return next();
         }
+        req.flash("message", "El usuario no existe");
         return res.redirect('/signin');
     }
 };
